af2cc57e22b83eb441368a545604f325950f598d
ViggieSmalls <victormovileanu@gmail.com>
2019-03-13 01:12:43 +0100

    finished nr. 5

--- 5.py
+++ 5.py
@@ -1,21 +1,35 @@
-n = 10
+n = 20
+seen = set()
 
-def smallest_divisor(x):
-    for i in range(2,int(x**0.5)):
-        if x%i == 0:
-            return i
-    return x
 
-s = set()
+def factors(x):
+    result = {}
+    i = 2
+    while i * i <= x:
+        if x % i:
+            i += 1
+        else:
+            if i in result:
+                result[i] += 1
+            else:
+                result[i] = 1
+            x //= i
+    if x > 1:
+        if x in result:
+            result[x] += 1
+        else:
+            result[x] = 1
+    return result
 
-def prime_factors(x):
-    result = set()
-    d = smallest_divisor(x)
-    if d == x:
-        result.add(d)
-        return result
-    else:
-        return result | prime_factors(x/d)
 
-print(prime_factors(21))
-#for i in reversed(range(n)):
+r = {}
+for i in range(2, n+1):
+    r2 = factors(i)
+    for key, v in r2.items():
+        if r.get(key, 0) < r2[key]:
+            r[key] = r2[key]
+
+result = 1
+for k, v in r.items():
+    result = k**v * result
+print(result)
