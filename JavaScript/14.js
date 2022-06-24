0fb72b8540649a8fe27ab7a3213bdee7cc0a258b
Victor <victormovileanu@gmail.com>
2022-06-24 08:59:27 +0200

    rewrite #14 in Javascript with array, since hash map is not necessary here

--- JavaScript/14.js
+++ JavaScript/14.js
@@ -1,25 +1,25 @@
-const collatzSequences = new Map()
-collatzSequences[1] = [1]
+const cache = Array(1000000 + 1)
+cache[1] = 1
 
-function calcCollatzSequenceFor(n) {
+function calcCollatzSequenceLengthFor(n) {
     // if n is memoized return value
-    if (n in collatzSequences) return collatzSequences[n]
+    if (cache[n]) return cache[n]
     // if n is even: n -> n/2
     // if n is odd: n -> 3n + 1
     let result
     if (n % 2 === 0) {
-        result = [n].concat(calcCollatzSequenceFor(n / 2))
+        result = 1 + calcCollatzSequenceLengthFor(n / 2)
     } else {
-        result = [n].concat(calcCollatzSequenceFor(3 * n + 1))
+        result = 1 + calcCollatzSequenceLengthFor(3 * n + 1)
     }
-    collatzSequences[n] = result
+    cache[n] = result
     return result
 }
 
 let max = 0
 let result
 for (let i = 1; i < 1000000; i++) {
-    const length = calcCollatzSequenceFor(i).length
+    const length = calcCollatzSequenceLengthFor(i)
     if (max < length) {
         max = length
         result = i
