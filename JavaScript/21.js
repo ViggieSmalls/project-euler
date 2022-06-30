b357380564ae49e0117cdcb404240cdb2c47722c
Victor <victormovileanu@gmail.com>
2022-06-30 10:47:55 +0200

    export function and fix bug

--- JavaScript/21.js
+++ JavaScript/21.js
@@ -1,33 +1,38 @@
-function d(n) {
+function sumOfProperDivisors(n) {
+    const sqrt = Math.sqrt(n)
     let sum = 1  // start with 1
-    for (let i = 2; i < n ** 0.5; i++) {
+    for (let i = 2; i <= sqrt; i++) {
         if (n % i === 0) {
             sum += i
             sum += n / i
         }
     }
-    if (Number.isInteger(n ** 0.5)) sum -= n ** 0.5  // case n is cube
+    if (Number.isInteger(sqrt)) sum -= sqrt  // count square root once in sum of factors
     return sum
 }
+module.exports = sumOfProperDivisors
 
-const arr = new Array(10_000)
-const MAX = 10_000
-for (let i = 2; i < MAX; i++) {
-    if (arr[i] === undefined) {
-        let a = i
-        let b = d(a)
-        let c = d(b)
-        if (a !==b && a === c && a < MAX && b < MAX) {
-            arr[a] = true
-            arr[b] = true
-        } else {
-            arr[a] = false
+if (require.main === module) {
+    const arr = new Array(10_000)
+    const MAX = 10_000
+    for (let i = 2; i < MAX; i++) {
+        if (arr[i] === undefined) {
+            let a = i
+            let b = sumOfProperDivisors(a)
+            let c = sumOfProperDivisors(b)
+            if (a !== b && a === c && a < MAX && b < MAX) {
+                arr[a] = true
+                arr[b] = true
+            } else {
+                arr[a] = false
+            }
         }
     }
+
+    let result = 0
+    for (let i = 2; i < MAX; i++) {
+        if (arr[i]) result += i
+    }
+    console.log(result)
 }
 
-let result = 0
-for (let i = 2; i < MAX; i++) {
-    if (arr[i]) result += i
-}
-console.log(result)
